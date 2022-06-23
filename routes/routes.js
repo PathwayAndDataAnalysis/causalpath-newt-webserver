653bfd4dbd35b624e8e1ca5a7d28791fa3b77c1c
KisanThapa <kisanthapa33@gmail.com>
2022-06-23 18:03:36 -0400

    jsTree folder tree

--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', {title: 'CausalPath Webserver'});
+	res.render('index', { title: 'CausalPath Webserver' });
 });
 
 // Network Diagram
 router.get('/graph', (req, res, next) => {
-    res.render('graph', {title: "Graph Network"});
+	res.render('graph', { title: 'Graph Network' });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
  * Listen on provided PORT, on all network interfaces.
  */
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
 server.on('error', onError);
 server.on('listening', onListening);
@@ -31,55 +31,52 @@ server.on('listening', onListening);
  * Normalize a PORT into a number, string, or false.
  */
 function normalizePort(val) {
-    const port = parseInt(val, 10);
+	const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
 
-    if (port >= 0) {
-        // PORT number
-        return port;
-    }
+	if (port >= 0) {
+		// PORT number
+		return port;
+	}
 
-    return false;
+	return false;
 }
 
 /**
  * Event listener for HTTP server "error" event.
  */
 function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
 
-    const bind = typeof PORT === 'string'
-        ? 'Pipe ' + PORT
-        : 'Port ' + PORT;
+	const bind = typeof PORT === 'string' ? 'Pipe ' + PORT : 'Port ' + PORT;
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
 }
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'PORT ' + addr.port;
-    debug('Listening on ' + bind);
+	const addr = server.address();
+	const bind =
+		typeof addr === 'string' ? 'pipe ' + addr : 'PORT ' + addr.port;
+	debug('Listening on ' + bind);
 }
