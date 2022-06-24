0f8b47fb22a754e27dee2c75a4c6d79f893dd9d0
KisanThapa <kisanthapa33@gmail.com>
2022-06-24 17:27:20 -0400

    read folder tree

--- app.js
+++ app.js
@@ -4,48 +4,64 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-// init router
-const router = require('./routes/routes');
 const createError = require('http-errors');
-
 const app = express();
+const api = require('./lib/api');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // init middlewares
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({origin: true, credentials: true}));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use('/api', api);
+
 
 // init static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // user bootstrap styles
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
+
 // init routers
-app.use('/', router);
+/* GET home page. */
+app.get('/', (req, res, next) => {
+    res.render('index', {
+        title: 'CausalPath Webserver'
+    });
+});
+
+// Network Diagram
+app.get('/graph', (req, res, next) => {
+    res.render('graph', {
+        title: 'Graph Network',
+        graphTee: {}
+    });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,77 @@
-const redirectToGraph = () => {
+const graphChoiceEnum = {
+    JSON: 1, ANALYSIS: 2,
+    DEMO: 3,
+};
 
+let graphChoice = graphChoiceEnum.ANALYSIS;
+
+
+function buildFolderTree(paths, treeNode, file, parentNodePath = '') {
+
+    let idSeparator = '___';
+    if (paths.length === 0) {
+        return;
+    }
+
+    for (let i = 0; i < treeNode.length; i++) {
+        let nodeText = treeNode[i].text;
+        if (paths[0] === nodeText) {
+            buildFolderTree(
+                paths.splice(1, paths.length),
+                treeNode[i].children,
+                file,
+                parentNodePath + idSeparator + nodeText
+            );
+            return;
+        }
+    }
+
+    let nodeId = parentNodePath + idSeparator + paths[0];
+    let newNode = {
+        id: nodeId,
+        text: paths[0],
+        children: [],
+        state: {opened: true},
+        data: file,
+    };
+    treeNode.push(newNode);
+    buildFolderTree(paths.splice(1, paths.length), newNode.children, file, nodeId);
 }
 
-loadAnalysisDirFromClientInput = (event) => {
-    let fileList = Array.from(event.target.files);
-    console.log(fileList)
+/***
+ * Organizes data as a tree and displays the jstree associated with it
+ * @param fileList: List of files to display
+ * @param isFromClient: file list structure is different depending on whether it is coming from the server or client
+ */
+function buildAndDisplayFolderTree(fileList, isFromClient, chosenNodeId) {
+
+    let data = [];
+
+    fileList.forEach(file => {
+        let paths = file.webkitRelativePath.split('/').slice(0, -1);
+        buildFolderTree(paths, data, file)
+    })
+
+    let hierarchy = {core: {data: data}};
+
+    console.log(JSON.stringify(hierarchy))
 }
 
+/***
+ * Load graph directories as a tree in json format
+ * In visualize results from a previous analysis
+ */
+function loadAnalysisFilesFromClient(fileList, chosenNodeId) {
+    graphChoice = graphChoiceEnum.JSON;
+    this.buildAndDisplayFolderTree(fileList, true, chosenNodeId);
+}
+
+
+document.getElementById('graph-file-input')
+    .addEventListener('change', (event) => {
+        let files = event.target.files;
+
+        let fileList = Array.from(files);
+
+        this.loadAnalysisFilesFromClient(fileList);
+    });
