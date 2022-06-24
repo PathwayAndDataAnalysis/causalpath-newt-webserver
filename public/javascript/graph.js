f7981d6931c7932bb9f662ac47782fa402a7f013
KisanThapa <kisanthapa33@gmail.com>
2022-06-24 17:27:20 -0400

    read folder tree

--- public/javascript/graph.js
+++ public/javascript/graph.js
@@ -1,29 +1,106 @@
-console.log('this is js of graph');
+let handleResponse = (res, afterResolve, handleRequestError, getResData) => {
+    let {statusText, status, ok} = res;
+    if (!ok) {
+        let errStr = status + ' - ' + statusText;
+        return handleRequestError(errStr);
+    }
+    if (!getResData)
+        getResData = () => res.text();
+
+    return getResData(res).then(afterResolve);
+};
 
 $(function () {
-	$('#folder-tree-container').jstree({
-		core: {
-			data: [
-				{
-					text: 'Root node',
-					children: [
-						{ text: 'Child node 1' },
-						{ text: 'Child node 2' },
-						{
-							text: 'Child node 2',
-							children: [{ text: 'Child node 1' }, { text: 'Child node 2' }],
-						},
-						{ text: 'Child node 2' },
-						{ text: 'Child node 2' },
-					],
-				},
-			],
-		},
-	});
+    $('#folder-tree-container').jstree({
+        "core":
+            {
+                "data":
+                    [{
+                        "id": "___T-TPC",
+                        "text": "T-TPC",
+                        "children": [{
+                            "id": "___T-TPC___CausalPath-rank-based",
+                            "text": "CausalPath-rank-based",
+                            "children": [],
+                            "state": {"opened": true},
+                            "data": {}
+                        }, {
+                            "id": "___T-TPC___CausalPath-classical",
+                            "text": "CausalPath-classical",
+                            "children": [{
+                                "id": "___T-TPC___CausalPath-classical___.panda",
+                                "text": ".panda",
+                                "children": [{
+                                    "id": "___T-TPC___CausalPath-classical___.panda___SignedPC",
+                                    "text": "SignedPC",
+                                    "children": [],
+                                    "state": {"opened": true},
+                                    "data": {}
+                                }, {
+                                    "id": "___T-TPC___CausalPath-classical___.panda___PC",
+                                    "text": "PC",
+                                    "children": [],
+                                    "state": {"opened": true},
+                                    "data": {}
+                                }],
+                                "state": {"opened": true},
+                                "data": {}
+                            }],
+                            "state": {"opened": true},
+                            "data": {}
+                        }, {
+                            "id": "___T-TPC___.panda",
+                            "text": ".panda",
+                            "children": [{
+                                "id": "___T-TPC___.panda___SignedPC",
+                                "text": "SignedPC",
+                                "children": [],
+                                "state": {"opened": true},
+                                "data": {}
+                            }, {
+                                "id": "___T-TPC___.panda___PC",
+                                "text": "PC",
+                                "children": [],
+                                "state": {"opened": true},
+                                "data": {}
+                            }],
+                            "state": {"opened": true},
+                            "data": {}
+                        }],
+                        "state": {"opened": true},
+                        "data": {}
+                    }]
+            }
+    });
 
-	// jstree node click event
-	$('#folder-tree-container').on('changed.jstree', function (e, data) {
-		console.log('The selected nodes are:');
-		console.log(data.selected);
-	});
+// jstree node click event
+    $('#folder-tree-container').on('dblclick.jstree', function (e, data) {
+
+        const instance = $('#folder-tree-container').jstree(true);
+        let node = instance.get_node(e.target)
+        let file = node.data
+
+        // make requests
+        let makeRequest = () =>
+            fetch('/api/getJsonAtPath', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(file),
+            });
+
+        let afterResolve = (fileContent) => {
+            console.log(fileContent)
+        };
+
+        let handleRequestError = (err) => {
+            alert('The error message is:\n' + err);
+            throw err;
+        };
+        makeRequest().then((res) =>
+            handleResponse(res, afterResolve, handleRequestError)
+        );
+
+    });
 });
