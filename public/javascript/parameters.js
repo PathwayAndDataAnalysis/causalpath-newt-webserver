4f7fe7b90e5e109a8218ec37a39b64e5788ae944
KisanThapa <kisanthapa33@gmail.com>
2022-07-26 14:37:53 -0400

    Custom parameters

---
+++ public/javascript/parameters.js
@@ -0,0 +1,9 @@
+const enableTopologyGrouping = true;
+const idealEdgeLength = 60;
+const tilingVerticalPadding = 5;
+const tilingHorizontalPadding = 5;
+
+exports.enableTopologyGrouping = enableTopologyGrouping;
+exports.idealEdgeLength = idealEdgeLength;
+exports.tilingVerticalPadding = tilingVerticalPadding;
+exports.tilingHorizontalPadding = tilingHorizontalPadding;
